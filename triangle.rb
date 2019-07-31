07da62670adaf17eaa809f209012f0ed2a54744a
Salvatore Santamaria <sam.santamaria@gmail.com>
2019-07-31 11:36:28 -0700

    finished triangle project 2

--- triangle.rb
+++ triangle.rb
@@ -13,7 +13,30 @@
 # and
 #   about_triangle_project_2.rb
 #
+
+
+
+
 def triangle(a, b, c)
+
+  if a == 0 || b == 0 || c == 0 
+    raise TriangleError.new("Sides can't be 0")
+  end
+
+  if a < 0 || b < 0 || c < 0 
+    raise TriangleError.new("Sides can't be negative")
+  end
+
+
+  if a + b <= c || a + c <= b || b + c <= a
+    raise TriangleError.new("Sides must be greater")
+  end
+
+  # if a + c == 0 
+  #   raise TriangleError.new("Sides must be greater")
+  # end
+
+
   if (a == b) && (a == c)
     :equilateral 
   elsif a != b && a != c && b != c
