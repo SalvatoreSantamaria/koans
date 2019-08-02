c4b2b88c0a23cbc64f88e8ca6d8d408d1312f9f1
Salvatore Santamaria <sam.santamaria@gmail.com>
2019-08-02 11:18:15 -0700

    Finished About Scoring Project

--- about_scoring_project.rb
+++ about_scoring_project.rb
@@ -29,63 +29,121 @@ require File.expand_path(File.dirname(__FILE__) + '/neo')
 #
 # Your goal is to write the score method.
 
+###
+##dice = [1,1,1,5,1]
+
+
+
 def score(dice)
   # You need to write this method
-
+  total_score = 0
   if dice == [] 
-    return 0
+  #   puts "there are no dice"  
+      return 0
+  elsif dice != []
+      ones_counter = 0 
+      twos_counter = 0
+      threes_counter = 0
+      fours_counter = 0
+      fives_counter = 0
+      sixes_counter = 0
+     # array_counter = 0
+          dice.each do |i|
+              if i == 1
+                  ones_counter += 1
+              end
+              if i == 2
+                  twos_counter += 1
+              end
+              if i == 3
+                  threes_counter += 1
+              end
+              if i == 4
+                  fours_counter += 1
+              end
+              if i == 5 
+                  fives_counter += 1
+              end
+              if i == 6 
+                  sixes_counter += 1
+              end
+          end
+      
+          if ones_counter <= 2 
+              total_score += ones_counter * 100
+              #puts ones_counter
+              #puts total_score
+          end
+          # if ones_counter == 3
+          #     total_score += 1000
+          #     #puts total_score
+          # end
+          if ones_counter >= 4
+              ones_counter -= 3
+             total_score += 1000
+              total_score += ones_counter * 100
+              #puts total_score
+          end
+
+          if fives_counter == 3 
+
+              # fives_counter = fives_counter - 3
+              total_score += 500
+          end   
+
+          if fives_counter <= 2 
+              total_score += fives_counter * 50
+          end
+
+          if fives_counter >= 4
+              total_score += 500
+              fives_counter -= 3
+              total_score += fives_counter * 50
+          end
+
+          # if fives_counter != 3
+          #     total_score += fives_counter * 50
+          #     #puts fives_counter
+          # end
+
+          if ones_counter == 3 
+              total_score += 1000
+           #   return "from 1000 counters #{ones_counter twos_counter threes_counter fours_counter fives_counter}"
+          end
+
+          if twos_counter == 3 
+              total_score += 200
+          end   
+          if threes_counter == 3 
+              total_score += 300
+          end   
+          if fours_counter == 3 
+              total_score += 400
+          end  
+          if sixes_counter == 3 
+              total_score += 600
+          end   
+
+          return total_score;
+
+
+      
   end
- score = 0
- dice.sort
-
-loop while a > dice.length
-  a += 1
-  fives_counter = 0
-  if dice[a] == 5
-    fives_counter += 1
-  if fives_counter == 1
-   return 50
-  end 
+  #  end
+  #pseudo code
+  # check to see dice array is empty
+  # loop thru array
+  # check to see how many five exist that are not 3
+  # check to see how many onces exits that are not 3
+  # check to see if three numbers that are not ones exist
+  # check to see if three ones exist
+  
+  
 end
- #if dice.includes!(5) && includes()
- 
- 
-#pseudo
-# check to see dice array is empty
-# loop thru array
-# check to see if a single five exists
-# check to see if a single one exists
-# check to see if three numbers that are not ones exist
-# check to see if three ones exist
+###
 
 
 
-
-  # score = 0
-  # sorted_dice = dice.sort # Sort the rolls
-
-  # while sorted_dice.length > 0
-  #   val = sorted_dice.shift
-  #   puts val
-  #   if sorted_dice.length >= 2 && val == sorted_dice[0] && val == sorted_dice[1]  # a triple!
-  #     score += val == 1 ? 1000 : val * 100
-  #     sorted_dice.shift(2)
-  #   else
-  #     score += 100 if val == 1
-  #     score += 50  if val == 5
-  #   end
-  # end
-
-  return score
-
-
-
-
-
-
-
-
-end
 
 class AboutScoringProject < Neo::Koan
   def test_score_of_an_empty_list_is_zero
--- scoring_workfile.rb
+++ scoring_workfile.rb
@@ -2,7 +2,8 @@
 # dice = gets.chomp
 # puts "Dice are #{dice}"
 
-dice = [1,1,4,5,5]
+###
+dice = [5,5,5]
 
 
 
@@ -10,7 +11,7 @@ def score(dice)
     # You need to write this method
     total_score = 0
     if dice == [] 
-      puts "there are no dice"  
+    #   puts "there are no dice"  
         return 0
     elsif dice != []
         ones_counter = 0 
@@ -18,6 +19,7 @@ def score(dice)
         threes_counter = 0
         fours_counter = 0
         fives_counter = 0
+        sixes_counter = 0
        # array_counter = 0
             dice.each do |i|
                 if i == 1
@@ -35,21 +37,65 @@ def score(dice)
                 if i == 5 
                     fives_counter += 1
                 end
+                if i == 6 
+                    sixes_counter += 1
+                end
             end
         
-            if ones_counter <= 2
+            if ones_counter <= 2 
                 total_score += ones_counter * 100
                 #puts ones_counter
                 #puts total_score
             end
-            if ones_counter == 3
-                total_score += 1000
+            # if ones_counter == 3
+            #     total_score += 1000
+            #     #puts total_score
+            # end
+            if ones_counter >= 4
+                ones_counter -= 3
+               total_score += 1000
+                total_score += ones_counter * 100
                 #puts total_score
             end
-            if fives_counter != 3
+
+            if fives_counter == 3 
+
+                # fives_counter = fives_counter - 3
+                total_score += 500
+            end   
+
+            if fives_counter <= 2 
                 total_score += fives_counter * 50
-                #puts fives_counter
             end
+
+            if fives_counter >= 4
+                total_score += 500
+                fives_counter -= 3
+                total_score += fives_counter * 50
+            end
+
+            # if fives_counter != 3
+            #     total_score += fives_counter * 50
+            #     #puts fives_counter
+            # end
+
+            if ones_counter == 3 
+                total_score += 1000
+             #   return "from 1000 counters #{ones_counter twos_counter threes_counter fours_counter fives_counter}"
+            end
+
+            if twos_counter == 3 
+                total_score += 200
+            end   
+            if threes_counter == 3 
+                total_score += 300
+            end   
+            if fours_counter == 3 
+                total_score += 400
+            end  
+            if sixes_counter == 3 
+                total_score += 600
+            end   
 
             return total_score;
 
@@ -57,15 +103,15 @@ def score(dice)
         
     end
   #  end
-  #pseudo
+  #pseudo code
   # check to see dice array is empty
   # loop thru array
-  # check to see if a single five exists
-  # check to see if a single one exists
+  # check to see how many five exist that are not 3
+  # check to see how many onces exits that are not 3
   # check to see if three numbers that are not ones exist
   # check to see if three ones exist
   
   
 end
-
+###
   puts score(dice)
